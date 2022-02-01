f1a6945bc72b4e820d784891b6c0afdc94286464
Ashton Bravo <Ashton.Bravo@vu.com>
2022-02-01 13:52:18 -0600

    attempt 1

--- payCalculator.py
+++ payCalculator.py
@@ -2,7 +2,11 @@ def calculatePay():
     # Implement your solution in between the two comment blocks
     print("calculating pay")
     # This first line is provided for you
-    hrs = input("Enter Hours:")
+    
+hrs = input("Enter Hours:")
+    Rate = input("Enter Rate:")
+    Gross Pay = hrs * Rate
+    print (gross pay)
     
     # end assignment
 
